95988a3fe5f858e88019c1a249c24a81fc249fea
sebastianrost <sebastianrost1990@gmail.com>
2022-08-22 22:41:16 -0300

    Ejercicio 2 resuelto

--- Ejercicio2/Program.cs
+++ Ejercicio2/Program.cs
@@ -6,7 +6,24 @@ namespace Ejercicio2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Ingresar un número y mostrar el cuadrado y el cubo del mismo.
+            //Se debe validar que el número sea mayor que cero,
+            //caso contrario, mostrar el mensaje: "ERROR. ¡Reingresar número!"
+
+            int numero;
+            double cuadrado;
+            double cubo;
+            Console.WriteLine("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+            while (numero < 1)
+            {
+                Console.WriteLine("ERROR. ¡Reingresar número!");
+                numero = int.Parse(Console.ReadLine());
+            }
+            cuadrado = numero * numero;
+            cubo = cuadrado * numero;
+            Console.WriteLine("El cuadrado del numero es: {0} y el cubo: {1} ", cuadrado, cubo);
+
         }
     }
 }
